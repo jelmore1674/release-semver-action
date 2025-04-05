6571d309329f380fed31b3bc866083a36cede275
Justin Elmore <jelmore1674@pm.me>
2025-04-04 23:09:30 -0500

    test npm version exec

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { execSync } from "node:child_process";
+
 function run() {
+  const newVersion = execSync("npm version patch", { encoding: "utf8" });
+  console.log({ newVersion });
 }
 
 run();

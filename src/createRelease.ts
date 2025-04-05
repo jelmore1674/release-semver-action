a4f71c42a31028167111445aab9ca4122f07f0b5
Justin Elmore <jelmore1674@pm.me>
2025-04-05 00:52:03 -0500

    stub out action

--- src/createRelease.ts
+++ src/createRelease.ts
@@ -1,17 +1,50 @@
-import { getInput } from "@actions/core";
+import { getInput, setFailed, setOutput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
+import { exit } from "node:process";
+import { inc } from "semver";
+import type { ReleaseType } from "./npmVersion";
 
-async function createRelease() {
+async function createRelease(releaseType: ReleaseType, version?: string) {
   const { owner, repo } = context.repo;
   const githubToken = getInput("token", { required: true });
+  const body = getInput("body", { required: false });
   const github = getOctokit(githubToken);
+  const target_commitish = getInput("commitish", { required: false });
+  const generate_release_notes = Boolean(body);
 
-  // target_commitish
-  await github.rest.repos.createRelease({
+  let tag_name = version;
+
+  if (!tag_name) {
+    const latestRelease = await github.rest.repos.getLatestRelease({ owner, repo });
+
+    if (!latestRelease) {
+      throw Error("No tag");
+    }
+
+    const newTag = inc(latestRelease.data.tag_name, releaseType);
+    if (!newTag) {
+      setFailed("Unable to bump the latest version.");
+      exit(1);
+    }
+
+    tag_name = `v${newTag}`;
+  }
+
+  const createdRelease = await github.rest.repos.createRelease({
     owner,
     repo,
-    tag_name: "v0.0.1",
+    tag_name,
+    name: tag_name,
+    body,
+    target_commitish,
+    generate_release_notes,
   });
+
+  const { data: { id, html_url, upload_url } } = createdRelease;
+
+  setOutput("release_id", id);
+  setOutput("html_url", html_url);
+  setOutput("upload_url", upload_url);
 }
 
 export { createRelease };
